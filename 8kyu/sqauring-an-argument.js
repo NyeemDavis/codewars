48cd6baabee413ed53d1b0dc8706f36638de6ef2
Nyeem Davis <nyeemdavis4@gmail.com>
2022-12-18 00:51:20 -0500

    added sqauring an arg codewar

---
+++ 8kyu/sqauring-an-argument.js
@@ -0,0 +1,15 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+//PREP
+// P: number
+// R: number, +
+// E:
+
+squared(3) // 9
+squared(9) // 81
+
+
+function squared(n) {
+    return n * n 
+}
