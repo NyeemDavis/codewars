7cc369d69994ee2be8e53d7f612ed02b035e0c26
Nyeem Davis <nyeemdavis4@gmail.com>
2022-12-23 06:48:44 -0500

    added bouncing balls codewar

---
+++ 6kyu/bouncing-balls.js
@@ -0,0 +1,38 @@
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+
+// PREP
+// P  number, float or whole, always +
+// R: number, float or whole. -1 if checks arent passed
+// E:
+bouncingBall(30, .66, 1.5) // 3
+
+// My solution 
+
+function bouncingBall(h, bounce, window) {
+    // Check for everything that returns false
+    if(h == 0 || (bounce > 1 || bounce < 1) || window > h) {
+        console.log(false)
+    }
+    // The mom will always see the ball at least(Initial fall)
+    // Loop
+    let seen = 0
+    seen 
+    while(h > window) {
+        h = h * bounce
+        seen += 1
+    }
+    console.log(seen)
+}
