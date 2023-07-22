5e3539f4aaee3e0feb936e35443c6e0a8914ea98
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-22 07:05:29 -0400

    sdada

--- newshit/7kyu.js
+++ newshit/7kyu.js
@@ -5,8 +5,8 @@
   // Avoid using datetime libraries
 
 // Examples
-  //getMilitaryTime('12:00:01AM'), '00:00:01'
-  //getMilitaryTime('11:46:47PM'), '23:46:47')
+  //getMilitaryTime('12:00:01AM'), -> '00:00:01'
+  //getMilitaryTime('11:46:47PM'), -> '23:46:47')
 
 // Notes
   // Only thing changing, is the hours
@@ -21,28 +21,37 @@
     // Return the hours + the rest of the time
     const getMilitaryTime = (input) => {
         // Get time into an array without the time period
+          // get the time substring itself without am or pm, start at 0 and move up until we are two indexed away from the end
+            // split into an array by the ":" to make 12:00:00AM -> [12, 00, 00]
         const time = input.substring(0, input.length - 2).split(':');
         // Figure out what time period we are in
+          // Use substring and start two indexes from end(AM/PM will always be in those posistions)
         const timePeriod = input.substring(input.length - 2);
         // Varibales to hold hours, and the rest of the time
         let hours = '00';
+
+        // The rest of the time not including the hours since we arent touching those
         const restOfTime = time.slice(1);
         
         // Conditional on what to do based on time period
+          // If it is PM
         if(timePeriod == 'PM') {
-          // 12 is the start of PM, so anything after will just be 12 + the hour
+          // 12 is the start of PM, so anything after will just be the hour added to 12
           hours = 12 + Number(time[0])
         } else {
-          // It is AM so just make the hours the same
+          // Quite literally nothing is done to morning times so hours just equals the hour given
           hours = time[0]
         };
+
         // Special case of 12 o'clock
+          // If it is 12 AM, The hour is always 00 then add rest of time
         if(time[0] == '12' && timePeriod == "AM") {
           return '00:' + restOfTime.join(':')
+          // If its 12 PM, time is still 12, we do not add anything to hours, add the rest of the time after
         } else if (time[0] === '12' && timePeriod == 'PM') {
           return '12:' + restOfTime.join(':')
         };
-          // Return the hours + the rest of the time
+          // Return our brand new military time
         return hours + ':' + restOfTime.join(':');
       }
       
@@ -70,8 +79,9 @@
     // Create a variable to hold the amount of dots on the top of the dice
     // Subtract the topNum from the total amount of dots
 
-const totalAmountVisible = (topNum, dice) => {
+const totalAmountVisible = (dice, topNum) => {
     let totalDots = 0
+    // Map for the total dots of each kind of dice
     const diceSides = {
         6: 21,
         8: 36,
@@ -79,7 +89,9 @@ const totalAmountVisible = (topNum, dice) => {
         12: 78,
         20: 210
     }
+    // The oppsite number is always the total of the sides plus 1, minus the number we rolled
     const opposite = dice + 1 - topNum
+    // Our total amount of dots, is the kind of dice minus the opposite number. Since that is the number face down on the table
     totalDots = diceSides[dice] - opposite
     return totalDots
 }
